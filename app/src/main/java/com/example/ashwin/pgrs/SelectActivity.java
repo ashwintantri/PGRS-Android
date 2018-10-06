184c43cf5041c026a8147af271da7713f4bab5ac
ashwintantri <ashwin.tantri@somaiya.edu>
2018-10-06 10:54:14 +0530

    added maps

--- app/src/main/java/com/example/ashwin/pgrs/SelectActivity.java
+++ app/src/main/java/com/example/ashwin/pgrs/SelectActivity.java
@@ -1,27 +1,41 @@
 package com.example.ashwin.pgrs;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -37,8 +51,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
-public class SelectActivity extends AppCompatActivity
-{
+public class SelectActivity extends AppCompatActivity implements OnMapReadyCallback{
     TextView viewComplaint;
     RecyclerView rv;
     DatabaseReference db;
@@ -46,6 +59,8 @@ public class SelectActivity extends AppCompatActivity
     FirebaseUser currentUser;
     SwipeRefreshLayout mSwipe;
     Query query;
+    ViewPager viewPager;
+    GoogleMap googleMap;
     ProgressBar progressBar;
     private FirebaseAuth mAuth;
     double lat,longitude;
@@ -57,32 +72,31 @@ public class SelectActivity extends AppCompatActivity
         setContentView(R.layout.select_activity);
         getSupportActionBar().setTitle("Submitted Complaints");
         mAuth = FirebaseAuth.getInstance();
+        Bundle mapViewBundle = null;
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(SelectActivity.this);
-        progressBar = findViewById(R.id.progress_id);
+        //progressBar = findViewById(R.id.progress_id);
         currentUser = mAuth.getCurrentUser();
-        mSwipe = findViewById(R.id.swipe_refresh);
 
+        final SupportMapFragment supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.maps_list_id);
+        supportMapFragment.getMapAsync(SelectActivity.this);
+        //mSwipe = findViewById(R.id.swipe_refresh);
         cr = new ArrayList<>();
         //db = FirebaseDatabase.getInstance().getReference();
-        rv = findViewById(R.id.complaint_recyclerview_id);
-        rv.setLayoutManager(new LinearLayoutManager(SelectActivity.this));
-        complaintsAdapter = new ComplaintsAdapter(SelectActivity.this,cr);
-        rv.setAdapter(complaintsAdapter);
+        //rv = findViewById(R.id.complaint_recyclerview_id);
+        //rv.setLayoutManager(new LinearLayoutManager(SelectActivity.this));
+        //complaintsAdapter = new ComplaintsAdapter(SelectActivity.this,cr);
+        //rv.setAdapter(complaintsAdapter);
         ActivityCompat.requestPermissions(SelectActivity.this,new String[]
                 {Manifest.permission.ACCESS_FINE_LOCATION},1);
         query = FirebaseDatabase.getInstance().getReference();
         query.addListenerForSingleValueEvent(vEl);
         //startActivity(getIntent());
-        mSwipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                mSwipe.setRefreshing(false);
-                query.addListenerForSingleValueEvent(vEl);
-                complaintsAdapter.notifyDataSetChanged();
-            }
-        });
-        complaintsAdapter.notifyDataSetChanged();
+
+        //complaintsAdapter.notifyDataSetChanged();
     }
+
+
+
     private double getDistance(double LAT1, double LONG1, double LAT2, double LONG2)
     {
         return 2 * 6371000 * Math.asin(Math.sqrt(Math.pow((Math.sin((LAT2 * (3.14159 / 180) - LAT1 * (3.14159 / 180)) / 2)), 2) + Math.cos(LAT2 * (3.14159 / 180)) * Math.cos(LAT1 * (3.14159 / 180)) * Math.sin(Math.pow(((LONG2 * (3.14159 / 180) - LONG1 * (3.14159 / 180)) / 2), 2))));
@@ -94,17 +108,18 @@ public class SelectActivity extends AppCompatActivity
         {
             //Toast.makeText(SelectActivity.this,"Changed",Toast.LENGTH_SHORT).show();
 
-            progressBar.setVisibility(View.GONE);
+            //progressBar.setVisibility(View.GONE);
             cr.clear();
             for(DataSnapshot complaintSnapshot : dataSnapshot.getChildren())
             {
                 Complaints c = complaintSnapshot.getValue(Complaints.class);
-                if(getDistance(c.getLat(),c.getLongitude(),lat,longitude)<300)
+                if(getDistance(c.getLat(),c.getLongitude(),lat,longitude)<600) {
                     cr.add(c);
+                    //googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat,longitude),14.0f));
+                    googleMap.addMarker(new MarkerOptions().position(new LatLng(c.getLat(),c.getLongitude())).title("Department: "+c.getDept()+"\n"+"Details: "+c.getDetails()+"\n"+"Status: "+c.getStatus()+"\n"+"Authority: "+c.getAuthority()));
+                }
             }
-            complaintsAdapter.swapItems(cr);
-            complaintsAdapter.notifyDataSetChanged();
-            Collections.reverse(cr);
+            //Collections.reverse(cr);
             query.addListenerForSingleValueEvent(vEl);
         }
 
@@ -142,6 +157,10 @@ public class SelectActivity extends AppCompatActivity
                                         {
                                             lat = location.getLatitude();
                                             longitude = location.getLongitude();
+                                            googleMap.setMinZoomPreference(16);
+                                            LatLng currentLoc = new LatLng(lat,longitude);
+                                            googleMap.addMarker(new MarkerOptions().position(currentLoc).title("My Location"));
+                                            googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLoc));
                                             Toast.makeText(SelectActivity.this,"Location detected",Toast.LENGTH_SHORT).show();
                                         }
                                     }
@@ -197,5 +216,33 @@ public class SelectActivity extends AppCompatActivity
         Intent startNewComplaint = new Intent(this, newComplaintActivity.class);
         startActivity(startNewComplaint);
         finish();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        this.googleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                Context context = getApplicationContext();
+                LinearLayout info = new LinearLayout(context);
+                info.setOrientation(LinearLayout.VERTICAL);
+                TextView title = new TextView(context);
+                title.setTextColor(Color.BLACK);
+                title.setGravity(Gravity.CENTER);
+                title.setTypeface(null, Typeface.BOLD);
+                title.setText(marker.getTitle());
+
+
+                info.addView(title);
+
+                return info;
+            }
+        });
     }
 }
